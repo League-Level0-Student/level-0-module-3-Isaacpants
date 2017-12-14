d226f2d206acf6d95974533d8216ec63016e51dc
Love4653 <league@WTS21.attlocal.net>
2017-12-13 18:29:06 -0800

    horoscope

---
+++ src/Horoscope.java
@@ -0,0 +1,54 @@
+import javax.swing.JOptionPane;
+
+public class Horoscope {
+public static void main(String[] args) {
+	
+	String sign = JOptionPane.showInputDialog("What is ur sign");
+	
+	if(sign.equals ("Aquarius")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day ");
+		
+	}
+	else if(sign.equals ("Pisces")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Aries")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+}
+	else if(sign.equals ("Taurus")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+}
+	else if(sign.equals ("Gemini")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Cancer")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Leo")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Virgo")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Libra")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Scorpio")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Sagittarius")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	}
+	else if(sign.equals ("Capricorn")) {
+		JOptionPane.showMessageDialog(null, "U Will die 1 day");
+	
+	}else {
+		JOptionPane.showMessageDialog(null, "Thats not a sign");
+		
+	}
+	
+	
+	
+	
+}
+}
